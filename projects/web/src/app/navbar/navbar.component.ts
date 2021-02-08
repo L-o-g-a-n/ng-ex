f96ffe6fdb6f399c65bc17ceca46e6409ed733d1
L-o-g-a-n <xy.logandev@gmail.com>
2021-02-08 16:42:17 +0900

    update

--- projects/web/src/app/navbar/navbar.component.ts
+++ projects/web/src/app/navbar/navbar.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  Menu1 = {
+    name: 'Menu1111111',
+    link: 'https://naver.com'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
